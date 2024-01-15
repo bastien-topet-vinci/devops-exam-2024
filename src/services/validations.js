1d522f9da51550df2146e1e718581df004a320bb
Topet <bastien.topet@student.vinci.be>
2024-01-15 17:17:08 +0100

    add : code to pass the third condition

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,6 +1,7 @@
 // TODO: Refactor validation function to follow the defined validations rules
 const isValid = (gamertag) =>{
     const specialCharacter = ['&','$','!','è','§','à','_'];
+    const numbers = [1,2,3,4,5,6,7,8,9,0]
     for (let i = 0; i< 7; i++){
         if (gamertag.includes(specialCharacter[i])){
             break;
@@ -12,7 +13,16 @@ const isValid = (gamertag) =>{
     if (gamertag.length < 8){
         return false;
     }
+    for (let i = 0; i< 10; i++){
+        if (gamertag.includes(numbers[i])){
+            break;
+        }
+        if (i === 9){
+            return false;
+        }
+    }
     return true;
+
 
 }
 
