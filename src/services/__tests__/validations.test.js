ed6bcc90b341c1fd8f5b8b1c5de2cde3af0a7427
Topet <bastien.topet@student.vinci.be>
2024-01-15 16:52:43 +0100

    add : test for special character

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -2,13 +2,12 @@
 const { isValid } = require('../validations');
 
 describe('validations tests suites - isValid', () => {
-    test('should return true or false as the function is defined', () => {
-        const result = isValid('');
-        expect(result).toBe(false);
-    });
-
-    test('should returns false', () => {
+    test('should return true', () => {
         const result = isValid('12345678');
+        expect(result).toBe(true);
+    });
+    test('should return true', () => {
+        const result = isValid('1234567&');
         expect(result).toBe(true);
     });
 });
