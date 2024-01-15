81bb7b57754aff84137417c39351a350b05abdf6
Topet <bastien.topet@student.vinci.be>
2024-01-15 17:13:45 +0100

    add : test for the third condition

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -3,11 +3,23 @@ const { isValid } = require('../validations');
 
 describe('validations tests suites - isValid', () => {
     test('should return true', () => {
-        const result = isValid('12345678');
+        const result = isValid('1&345678');
         expect(result).toBe(true);
     });
     test('should return true', () => {
         const result = isValid('1234567&');
+        expect(result).toBe(true);
+    });
+    test('should return false', () => {
+        const result = isValid('123');
+        expect(result).toBe(false);
+    });
+    test('should return false', () => {
+        const result = isValid('KidTaluva&');
+        expect(result).toBe(false);
+    });
+    test('should return true', () => {
+        const result = isValid('KidTaluva&1');
         expect(result).toBe(true);
     });
 });
--- src/services/validations.js
+++ src/services/validations.js
@@ -3,15 +3,16 @@ const isValid = (gamertag) =>{
     const specialCharacter = ['&','$','!','è','§','à','_'];
     for (let i = 0; i< 7; i++){
         if (gamertag.includes(specialCharacter[i])){
-        return true;
+            break;
+        }
+        if (i === 6){
+            return false;
         }
     }
-    
-
-    if (gamertag.length <= 8){
-        return true;
+    if (gamertag.length < 8){
+        return false;
     }
-    return false;
+    return true;
 
 }
 
